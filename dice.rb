e5b5adb73f2ef11ad9a8ef3c99c881f89a6f6de9
donnellycarroll <146589442+donnellycarroll@users.noreply.github.com>
2023-10-14 20:16:14 +0000

    add pages 10s

--- dice.rb
+++ dice.rb
@@ -1,8 +1,21 @@
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
   "Hello World"
+  "
+  <h1>Dice Roll</h1>
+  <ul>
+    <li><a href=\"/dice/2/6\">Roll the two 6-sided dice</a></li>
+    <li><a href=\"/dice/2/10\">Roll the two 10-sided dice</a></li>
+  </ul>
+  "
 end
 
 get("/zebra") do
@@ -24,5 +37,15 @@ get("/dice/2/6") do
 
   outcome = "You rolled a #{first_die} and a #{second_die} for a total sum of #{sum}"
   "<h1>2d6</h1>
+  <p>#{outcome}</p>"
+end
+
+get("/dice/2/10") do
+  first_die = rand(1..10)
+  second_die = rand(1..10)
+  sum = first_die + second_die
+
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total sum of #{sum}"
+  "<h1>2d10</h1>
   <p>#{outcome}</p>"
 end
