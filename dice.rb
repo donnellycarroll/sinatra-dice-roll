3fbf836ecc7e7c348a746bdded38f2ca44bbb49e
donnellycarroll <146589442+donnellycarroll@users.noreply.github.com>
2023-10-14 20:06:19 +0000

    2d6

--- dice.rb
+++ dice.rb
@@ -16,3 +16,13 @@ end
 get("/elle") do
   "This one too?"
 end
+
+get("/dice/2/6") do
+  first_die = rand(1..6)
+  second_die = rand(1..6)
+  sum = first_die + second_die
+
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total sum of #{sum}"
+  "<h1>2d6</h1>
+  <p>#{outcome}</p>"
+end
